2fce7d0cb3e05af6c4adfe4fbe81a6dbc2cada43
Peter Khalil <uni.03c9@gmail.com>
2022-03-16 14:10:35 +0200

    added eselect package for state memoization, and use it in cart.selectors

--- src/components/cart-icon/cart-icon.comp.jsx
+++ src/components/cart-icon/cart-icon.comp.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { toggleCartMenu } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 import { ReactComponent as ShoppingBagIcon } from "../../assets/shopping-bag.svg";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartMenu }) => (
+const CartIcon = ({ toggleCartMenu, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartMenu}>
     <ShoppingBagIcon className="shopping-icon" />
-    <span className="item-count"> 0 </span>
+    <span className="item-count"> {itemCount} </span>
   </div>
 );
 
@@ -17,4 +18,8 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartMenu: () => dispatch(toggleCartMenu()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
