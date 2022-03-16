a1494a7d19c6658400d75742df4d0f6280fcfbf3
Peter Khalil <uni.03c9@gmail.com>
2022-03-16 02:28:49 +0200

    deconstruct homepage into components, and generate MenuItems based on an array

---
+++ src/pages/homepage/homepage.comp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import DirectoryMenu from "../../components/directory-menu/directory-menu.comp";
+
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <DirectoryMenu />
+  </div>
+);
+
+export default HomePage;
