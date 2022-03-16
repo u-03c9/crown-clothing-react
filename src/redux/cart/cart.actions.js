bddeeadf157b08bab0829368b81a74df1f6e9b5c
Peter Khalil <uni.03c9@gmail.com>
2022-03-16 15:23:41 +0200

    added emoveWholeItemFromCart action

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,13 @@ export const addItemToCart = (item) => ({
   type: CartActionTypes.ADD_ITEM_TO_CART,
   payload: item,
 });
+
+export const removeWholeItemFromCart = (item) => ({
+  type: CartActionTypes.REMOVE_WHOLE_ITEM_FROM_CART,
+  payload: item,
+});
+
+export const removeSingleItemFromCart = (item) => ({
+  type: CartActionTypes.REMOVE_SINGLE_ITEM_FROM_CART,
+  payload: item,
+});
