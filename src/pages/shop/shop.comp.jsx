dfc9ec0a2437902b9c39e2e0015928aa30546b52
Peter Khalil <uni.03c9@gmail.com>
2022-03-16 19:03:13 +0200

    converting SHOP_DATA from an array to an object

--- src/pages/shop/shop.comp.jsx
+++ src/pages/shop/shop.comp.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import { selectCollections } from "../../redux/shop/shop.selector.js";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selector.js";
 import CollectionPreview from "../../components/collection-preview/collection-preview.comp.jsx";
 
 const ShopPage = ({ collections }) => (
@@ -14,7 +14,7 @@ const ShopPage = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(ShopPage);
